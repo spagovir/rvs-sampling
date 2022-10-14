4e456c98bfe87d315b539dc82f94ae27ed218fcd
Edgar Lin <edgarlin2000@gmaili.com>
2022-10-14 23:58:36 +0000

    some train loop stuff

--- rtg_replay_buffer.py
+++ rtg_replay_buffer.py
@@ -5,8 +5,21 @@ class RtGBuffer(circular_replay_buffer.OutOfGraphReplayBuffer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._cumulative_discount_vector[0] = 0
+    def __len__(self):
+        if self.is_full():
+            # add_count >= self._replay_capacity > self._stack_size
+            min_id = self.cursor() - self._replay_capacity + self._stack_size - 1
+            max_id = self.cursor() - self._update_horizon
+        else:
+            # add_count < self._replay_capacity
+            min_id = self._stack_size - 1
+            max_id = self.cursor() - self._update_horizon
+        return max_id - min_id
+
+
 
 # %%
+'''
 rb = RtGBuffer(
     observation_shape=(84,84), 
     stack_size = 4, 
@@ -31,11 +44,14 @@ i=0
 while True:
     states, ac, ret, next_states, _, _, terminal, indices = rb.sample_transition_batch(1,[i])
     _, _, _, _, _, next_reward, terminal, indices = rb2.sample_transition_batch(1, [i])
+    state_shape = states.shape
+    ac_shape = ac.shape
     ret = ret.item()
     next_reward = next_reward.item()
     terminal=terminal.item()
-    print(f"{ret=}, {next_reward=}, {terminal=}\n")
+    print(f"{state_shape=}, {ac_shape=}, {ret=}, {next_reward=}, {terminal=}\n")
     if terminal:
         break
     i += 1
 # %%
+'''
--- rvs.py
+++ rvs.py
@@ -1,9 +1,13 @@
+from argparse import Action
 from dataclasses import dataclass
+from typing import List
 from tqdm import tqdm
 import torch as t
 import torch.nn as nn
 from dataclasses import dataclass
 from torch.utils.data import DataLoader, Dataset, TensorDataset
+from rtg_replay_buffer import RtGBuffer
+from abc import ABC, abstractclassmethod
 
 """ Three separate MLPs for predicting P(A|R,S), P(A|S), and P(R|S)."""
 
@@ -13,11 +17,13 @@ class MLPConfig:
     num_state_dims : int 
     num_actions : int
     device : str
-    epochs : int
+    num_steps : int
+    steps_per_buffer : int
     lr : float = 0.0001
     intermediate_dim : int = 1024
     num_hidden_layers : int = 2
     batch_size = 1024
+    save_every = 10000
 
 class RvSMLP(nn.Module):
     def __init__(self, config : MLPConfig):
@@ -66,26 +72,54 @@ class RMLP(nn.Module):
             x = layer(x)
         return x
 
-def train(config : MLPConfig, dataset : Dataset, mainNet : RvSMLP, actionNet : AMLP, rewardNet : RMLP):
+class Binner(nn.Module):
+    def __init__(self, config : MLPConfig):
+        super().__init__()
+        self.register_buffer('running_min_max', t.tensor([0.0, 1.0]))
+        self.num_bins = config.num_reward_bins
+    def forward(self, x):
+        self.running_min_max[0] = min(self.running_min_max[0], t.min(x).item())
+        self.running_min_max[1] = max(self.running_min_max[1], t.max(x).item())
+        return t.floor((x - self.running_min_max[0])/(self.running_min_max[1]-self.running_min_max[0]) * self.num_bins).long()
+    def bin_value(self, n : int) -> float:
+        return self.running_min_max[0] + (self.running_min_max[1] - self.running_min_max[0]) / self.num_bins * n
+
+class RvSModel(nn.Module):
+    def __init__(self, config : MLPConfig):
+        super().__init__()
+        self.mainNet = RvSMLP(config)
+        self.actionNet = AMLP(config)
+        self.rewardNet = RMLP(config)
+        self.binner = Binner(config)
+    def forward(self, r, s):
+        binnedR = Binner(r)
+        aPredCond = self.mainNet(binnedR, s)
+        aPredUnCond = self.actionNet(s)
+        rPredUnCond = self.rewardNet(s)
+        return (binnedR, aPredCond, aPredUnCond,rPredUnCond)
+
+class TrainLogger:
+    def __init__(self):
+        
+    
+
+
+def train(config : MLPConfig, dataset : Dataset, model : RvSModel, saveDir : str):
     dl = DataLoader(dataset, MLPConfig.batch_size, True)
-    mainNet.to(config.device)
-    actionNet.to(config.device)
-    rewardNet.to(config.device)
-    mainOptim = t.optim.Adam(mainNet.parameters(), config.lr)
-    actionOptim = t.optim.Adam(actionNet.parameters(),config.lr)
-    rewardOptim = t.optim.Adam(rewardNet.parameters(), config.lr)
+    model.to(config.device)
+    mainOptim = t.optim.Adam(model.mainNet.parameters(), config.lr)
+    actionOptim = t.optim.Adam(model.actionNet.parameters(),config.lr)
+    rewardOptim = t.optim.Adam(model.rewardNet.parameters(), config.lr)
     loss = nn.CrossEntropyLoss()
-    for _ in tqdm(range(config.epochs)):
+    for _ in tqdm(range(config.num_steps)):
         for r, s, a in dl:
             r = r.to(config.device)
             s = s.to(config.device)
             a = a.to(config.device)
-            aPredCond = mainNet(r,s)
-            aPredUnCond = actionNet(s)
-            rPredUnCond = rewardNet(s)
+            binnedR, aPredCond, aPredUnCond, rPredUnCond = model(r,s)
             aPredCondLoss = loss(aPredCond, a)
             aPredUnCondLoss = loss(aPredUnCond, a)
-            rPredUnCondLoss = loss(rPredUnCond, r)
+            rPredUnCondLoss = loss(rPredUnCond, binnedR)
             mainOptim.zero_grad()
             actionOptim.zero_grad()
             rewardOptim.zero_grad()
@@ -96,3 +130,45 @@ def train(config : MLPConfig, dataset : Dataset, mainNet : RvSMLP, actionNet : A
             actionOptim.step()
             rewardOptim.step()
 
+
+def trainWDopamine(config : MLPConfig, game : str, model : RvSModel, logger : TrainLogger):
+    buffers = [
+        RtGBuffer(
+            observation_shape=(84,84), 
+            stack_size = 4, 
+            replay_capacity = 2000000, 
+            batch_size = 32,
+            update_horizon=200)
+        for i in range(50)
+    ]
+    for i in range(50):
+        buffers[i].load(f"./atari_data/dqn/{game}/1/replay_logs/", i)
+    model.to(config.device)
+    mainOptim = t.optim.Adam(model.mainNet.parameters(), config.lr)
+    actionOptim = t.optim.Adam(model.actionNet.parameters(),config.lr)
+    rewardOptim = t.optim.Adam(model.rewardNet.parameters(), config.lr)
+    loss = nn.CrossEntropyLoss()
+    for i in tqdm(range(config.num_steps)):
+        if i % config.steps_per_buffer == 0:
+            buffer_idx = t.randint(50,(1,)).item()
+        s, a, r, _, _, _, _, _ = buffers[buffer_idx].sample_transition_batch()
+        r = r.to(config.device)
+        s = s.to(config.device)
+        a = a.to(config.device)
+        binnedR, aPredCond, aPredUnCond, rPredUnCond = model(r,s)
+        aPredCondLoss = loss(aPredCond, a)
+        aPredUnCondLoss = loss(aPredUnCond, a)
+        rPredUnCondLoss = loss(rPredUnCond, binnedR)
+        mainOptim.zero_grad()
+        actionOptim.zero_grad()
+        rewardOptim.zero_grad()
+        aPredCondLoss.backward()
+        aPredUnCondLoss.backward()
+        rPredUnCondLoss.backward()
+        mainOptim.step()
+        actionOptim.step()
+        rewardOptim.step()
+        if i % config.log_steps == 0:
+            logger.log(i, aPredCondLoss, aPredUnCondLoss, rPredUnCondLoss)
+        if i % config.save_every == 0:
+            logger.save(model.state_dict())
